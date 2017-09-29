f0b40d370d43fba438be7319344368542d916fa4
William Rainaud <wrainaud@gmail.com>
2017-09-29 15:29:56 -0400

    Prereqs Complete

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'Qwh4uVEVYgx3bOGvUxh8id1XC',
+  consumer_secret: 'QUP5qYqwUWb6qmmgPw4C10eKukTt7FB5hrUS5FlrxFHjjF1rA7',
+  access_token_key: '3062175866-OJ8K3IO8IPaKxZwVqQNemnqugOKyEKkv2qIHriQ',
+  access_token_secret: 'pSfRehTf3mspq6ZYiq1e1KmMcqby8gTsSNG5QYGfRoxim',
+}
+
+module.exports = twitterKeys;
